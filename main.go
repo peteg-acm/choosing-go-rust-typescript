9f28ceb1b64da806fd42af4e18e9aed1e99f731d
Peter W. W. Gousios <peteg-acm@users.noreply.github.com>
2025-09-05 22:16:51 +0000

    added some additional changes and mutex

--- main.go
+++ main.go
@@ -2,32 +2,38 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 var curCount int
 var otherCount int
 var statsCount int
+var counterMutex = &sync.Mutex{}
 
 func handleRootRequest(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path == "/goodbye" {
+  log.Printf("%s, %s", r.Method, r.URL.Path)
+
+	if r.URL.Path == "/goodbye" {
 		os.Exit(0)
 	}
 
-	log.Printf("%s, %s", r.Method, r.URL.Path)
-
+	counterMutex.Lock()
+	defer counterMutex.Unlock()
 
 	if r.URL.Path == "/stats" {
-		statsCount++
+  	statsCount++
 		fmt.Fprintf(w, "<p>Stats: <br> calls to root %d <br> calls to stats %d <br> other calls %d <br> total calls %d",
 	              curCount, statsCount, otherCount, curCount+statsCount+otherCount)
+		time.Sleep(5 * time.Second)
 		return
 	}
 	if r.URL.Path != "/" {
 		otherCount++
 		fmt.Fprintf(w, "I am sorry but I can't find %s", r.URL.Path)
-		http.NotFound(w,r)
+		// http.NotFound(w,r)
 		return
 	}
 
@@ -40,6 +46,8 @@ func handleRootRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.Printf("Hello from Main()")
 	http.HandleFunc("/", handleRootRequest)
 	http.ListenAndServe(":8080", nil)
+	log.Printf("Goodbye!")
 }
